0453671bdf2b4ed8c6ebd238d60abb94f45e333a
brandon <brandonawilliams1@gmail.com>
2022-08-24 19:28:42 -0500

    added event listners

---
+++ script.js
@@ -0,0 +1,8 @@
+let add = document.querySelector("#add");
+let subract = document.querySelector("#subtract");
+
+console.log(add, subract);
+
+add.addEventListener("click", function () {
+    // Every time the add button is pressed, this code will execute.
+   });
