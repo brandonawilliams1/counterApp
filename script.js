5a22251164d5acf5de2c41ce0ae1e88318d61fdd
brandon <brandonawilliams1@gmail.com>
2022-08-24 19:37:43 -0500

    added callback function works

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let add = document.querySelector("#add");
 let subract = document.querySelector("#subtract");
-let result = Number(output.innerText) + 1;
+
 
 console.log(add, subract);
 
 add.addEventListener("click", function () {
     // Every time the add button is pressed, this code will execute.
     let output = document.querySelector("#output");
-    let result = output.innerText + 1;
+    let result = Number(output.innerText) + 1;
   
     output.innerText = result;
    });
