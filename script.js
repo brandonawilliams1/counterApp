d2cec41b7f6372ddcdc4e81915e88ca8d27160a8
brandon <brandonawilliams1@gmail.com>
2022-08-24 19:51:02 -0500

    added callback function to subtraction

--- script.js
+++ script.js
@@ -8,6 +8,21 @@ add.addEventListener("click", function () {
     // Every time the add button is pressed, this code will execute.
     let output = document.querySelector("#output");
     let result = Number(output.innerText) + 1;
+
+    if (result > 10) {
+        result = 0;
+      }
   
     output.innerText = result;
    });
+
+   subract.addEventListener("click", function () {
+    let output = document.querySelector("#output");
+    let result = Number(output.innerText) - 1;
+  
+    if (result < 0) {
+      result = 0;
+    }
+  
+    output.innerText = result;
+  });
